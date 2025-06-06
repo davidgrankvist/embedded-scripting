9a56416b7603b24dbb81145d1731c70530ce69d2
David Grankvist <grankvist.david@gmail.com>
2025-06-06 20:45:47 +0200

    add sandbox console read

--- GraphicsSandbox.App/UI/SandboxViewModel.cs
+++ GraphicsSandbox.App/UI/SandboxViewModel.cs
@@ -8,17 +8,20 @@ namespace GraphicsSandbox.App.UI
     {
         private readonly LuaEnvironment lua;
 
-        private string consoleOutput;
-        private bool isReadonly;
+        private string consoleText;
+        private bool isReadOnly;
         private bool shouldExecute;
         private Task? luaTask;
+
+        private SemaphoreSlim consoleReadSem;
+        private int consoleReadStart;
 
         public SandboxViewModel()
         {
             lua = new LuaEnvironment();
-            consoleOutput = string.Empty;
+            consoleText = string.Empty;
             Code = string.Empty;
-            isReadonly = true;
+            isReadOnly = true;
             shouldExecute = true;
             luaTask = null;
 
@@ -28,6 +31,14 @@ namespace GraphicsSandbox.App.UI
             lua.RegisterFunction(() => shouldExecute ? 1 : 0, "_ShouldExecute");
             // wrap with lua function to convert the int to a lua bool
             lua.TryExecute("cs.ShouldExecute = function () return cs._ShouldExecute() == 1 end", out _);
+
+            lua.RegisterFunction(() =>
+            {
+                BeginConsoleRead();
+                var subStrLen = consoleText.Length - consoleReadStart - Environment.NewLine.Length;
+                var result = consoleText.Substring(consoleReadStart, subStrLen);
+                return result;
+            }, "Readln");
         }
 
         public string Code { get; set; }
@@ -43,32 +54,51 @@ namespace GraphicsSandbox.App.UI
             });
         }
 
-        public string ConsoleOutput
+        public string ConsoleText
         {
-            get => consoleOutput; set
+            get => consoleText; set
             {
-                consoleOutput = value;
-                OnPropertyChanged(nameof(ConsoleOutput));
+                consoleText = value;
+                OnPropertyChanged(nameof(ConsoleText));
+
+                if (!IsReadOnly && consoleText.EndsWith(Environment.NewLine))
+                {
+                    EndConsoleRead();
+                }
             }
         }
 
-        public bool IsReadonly
+        public bool IsReadOnly
         {
-            get => isReadonly; set
+            get => isReadOnly; set
             {
-                isReadonly = value;
-                OnPropertyChanged(nameof(IsReadonly));
+                isReadOnly = value;
+                OnPropertyChanged(nameof(IsReadOnly));
             }
         }
 
         private void Print(string message)
         {
-            ConsoleOutput += message;
+            ConsoleText += message;
         }
 
         private void Println(string message)
         {
             Print(message + Environment.NewLine);
+        }
+
+        private void BeginConsoleRead()
+        {
+            consoleReadSem = new SemaphoreSlim(0);
+            consoleReadStart = consoleText.Length;
+            IsReadOnly = false;
+            consoleReadSem.Wait();
+        }
+
+        public void EndConsoleRead()
+        {
+            IsReadOnly = true;
+            consoleReadSem.Release();
         }
 
         public void Dispose()
