77543969294792e6f5664ce2d4920c7f6487592c
David Grankvist <grankvist.david@gmail.com>
2025-06-06 15:33:07 +0200

    add editor UI

---
+++ GraphicsSandbox.App/UI/EditorViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GraphicsSandbox.App.UI.Framework;
+
+namespace GraphicsSandbox.App.UI
+{
+    class EditorViewModel : ViewModelBase
+    {
+        private string code;
+
+        public EditorViewModel()
+        {
+            code = string.Empty;
+            RunCodeCommand = new DelegateCommand(OnRunCode);
+        }
+
+        public string Code
+        {
+            get => code; set
+            {
+                code = value;
+                OnPropertyChanged(nameof(Code));
+            }
+        }
+
+        public DelegateCommand RunCodeCommand { get; }
+
+        private void OnRunCode()
+        {
+            var window = new SandboxView();
+            window.Show();
+        }
+    }
+}
---
+++ GraphicsSandbox.App/UI/Framework/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace GraphicsSandbox.App.UI.Framework
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public DelegateCommand(Action execute)
+            : this(execute, () => true)
+        {
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute();
+        }
+    }
+}
